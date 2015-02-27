26e835aa505166fb699cd560e0527793d467a180
Vitalii Lakusta <lakusta96@gmail.com>
2015-02-27 18:38:52 +0200

    set up structure; still setting up first php files...

---
+++ public_html/index.html
@@ -0,0 +1,15 @@
+<?php 
+	require_once("/resources/config.php");
+
+	require_once(TEMPLATES_PATH . "/header.php");
+?>
+
+<div id="container">
+	<div id="content">
+		Content goes here...
+	</div>
+</div>
+
+<?php
+	require_once(TEMPLATES_PATH . "/footer.php");
+?>
---
+++ resources/templates/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<<html>
+<head lang="en">
+	<title>Usably</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+  <script type="text/javascript" src="js/jquery-1.11.2.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="header">
+	<h1>Usab.ly</h1>
+	<ul class="nav global">
+		<li><a href="#">Home</a></li>
+		<li><a href="#">About</a></li>
+		<li><a href="#">Contact us</a></li>
+	</ul>
+</div>
