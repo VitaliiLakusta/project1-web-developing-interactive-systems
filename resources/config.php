b2cef3ce0084981634ca88f4653e731695c36c26
Vitalii Lakusta <lakusta96@gmail.com>
2015-02-27 19:38:32 +0200

    set up config.php; before testing

---
+++ resources/config.php
@@ -0,0 +1,28 @@
+<?php 
+
+
+//TODO change db configuration after configuring XAMPP
+$config = array(
+	"db" => array(
+		"dbname" => "DB_NAME", 
+		"username" => "DB_USER",
+		"host" => "localhost"
+	),
+	"paths" => array(
+		"resources" => realpath(dirname(__FILE__)),
+		"images" => realpath(dirname(__FILE__) . "/../public_html/img")
+	)
+);
+
+defined("TEMPLATES_PATH")
+	or define ("TEMPLATES_PATH", realpath(dirname(__FILE__)) . "/templates");
+
+/* 
+	Error reporting 
+	// TODO read about this in detail
+*/
+ini_set("error_reporting", "true");
+error_reporting(E_ALL | E_STRICT);
+
+
+?>
